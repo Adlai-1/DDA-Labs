c4de03e6002c1083839c045463387fcfcd56600d
Adlai-1 <42801341+Adlai-1@users.noreply.github.com>
2020-03-11 05:00:08 +0000

    Update Selection sort.cpp

--- Selection sort.cpp
+++ Selection sort.cpp
@@ -8,3 +8,36 @@ void swap(int *xp, int *yp)
     *yp = temp;  
 }  
   
+void selectionSort(int arr[], int n)  
+{  
+    int i, j, min;  
+  
+    for (i = 0; i < n-1; i++)  
+    {  
+         
+        min = 0;  
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[min])  
+            min = j;  
+  
+          swap(&arr[min], &arr[i]);  
+    }  
+}  
+
+void Display(int arr[], int size)  
+{  
+    int i;  
+    for (i=0; i < size; i++)  
+        cout << arr[i] <<endl ;  
+     
+}  
+    
+int main()  
+{  
+    int arr[] = {64, 25, 12, 22, 11};  
+    int n = sizeof(arr)/sizeof(arr[0]);  
+    selectionSort(arr, n);  
+    cout << "Sorted array"<<endl;  
+    Display(arr, n);  
+    return 0;  
+}  
